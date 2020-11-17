807face8dbf42d1279b288b9c1b8eb2aa1de1df9
issone <269773075@qq.com>
2020-11-17 18:27:13 +0800

    旋转矩阵

---
+++ interview_question/ 01_07_旋转矩阵/solution.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        # 上下翻转
+        for i in range(n // 2):
+            matrix[i], matrix[n - i - 1] = matrix[n - i - 1], matrix[i]
+        # 主对角线翻转
+        for i in range(n):
+            for j in range(i):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
