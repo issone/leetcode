b6ab5c96b09bb9642f58f4e9f2ab1a1d626bc80e
issone <269773075@qq.com>
2020-12-11 17:23:30 +0800

    反转字符串

---
+++ 0344_reverse-string/solution.go
@@ -0,0 +1,9 @@
+package _328_odd_even_linked_list
+
+func reverseString(s []byte) {
+    for left, right := 0, len(s)-1; left < right; left++ {
+        s[left], s[right] = s[right], s[left]
+        right--
+    }
+}
+
