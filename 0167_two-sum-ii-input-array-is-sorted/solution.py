113a7b631fa1daccb9d6a85386b8090faa930cef
issone <269773075@qq.com>
2020-12-14 16:52:39 +0800

    两数之和哈希法

--- 0167_two-sum-ii-input-array-is-sorted/solution.py
+++ 0167_two-sum-ii-input-array-is-sorted/solution.py
@@ -13,11 +13,11 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         n = len(numbers)
         for i in range(n):
-            low, high = i + 1, n - 1
+            low, high = i + 1, n - 1    # low 开始的时候为i+1是为了，保证数组长度为偶数时，mid得到的是后面那个元素，避免mid和i重复
             while low <= high:
                 mid = (low + high) // 2
                 if numbers[mid] == target - numbers[i]:
-                    return [i + 1, mid + 1]
+                    return [i + 1, mid + 1]  # 因为返回的下表不是从0开始，所以在原基础上+1
                 elif numbers[mid] > target - numbers[i]:
                     high = mid - 1
                 else:
@@ -25,3 +25,21 @@ class Solution:
 
         return [-1, -1]
 
+
+class Solution2:
+    """
+     哈希表
+    首先设置字典dict={},然后通过enumerate函数对numbers迭代，index代表索引，num代表索引对应的值。
+    先看最后面，[target-num]作为dict的键，index作为值。
+    这样if判断只需要判断num是否在dict中，如果在的话，通过dict[num]找到之前的num对应的index，那么就可以返回[dict[num]+1,index+1],
+    不在的话就更新字典，target-num代表着下一个需要的数字，而迭代的num就是下一个数字，以此来判断。
+    """
+
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        dic = dict()
+        for i in range(len(numbers)):
+            tmp = target - numbers[i]
+            if tmp in dic:
+                return [dic[tmp] + 1, i + 1]
+            dic[numbers[i]] = i
+        return [-1, -1]
